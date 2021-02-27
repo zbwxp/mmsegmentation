488533180a86768e46ba0bec5230a89554f20a8f
zbwxp <zbwxpau@gmail.com>
2021-02-27 13:11:31 +1030

    updated ade20k train config

--- configs/deeppad/deeppad_r101_512x512_80k_ade20k.py
+++ configs/deeppad/deeppad_r101_512x512_80k_ade20k.py
@@ -1,4 +1,11 @@
 _base_ = './deeppad_r50_512x512_80k_ade20k.py'
-model = dict(pretrained='open-mmlab://resnet101_v1c',
-             backbone=dict(depth=101),
-             )
+model = dict(
+    pretrained='open-mmlab://resnet101_v1c',
+    backbone=dict(depth=101),
+    decode_head=dict(
+            type='DeepPadHead512',
+            dyn_branch_ch=8,
+            mask_head_ch=8,
+            pad_out_channel_factor=512/48,
+    ),
+)
