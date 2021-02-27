6472819aaafae5dafadefe578492b9583862ae98
zbwxp <zbwxpau@gmail.com>
2021-02-27 18:24:04 +1030

    updated pascal_context configs

--- configs/deeppad/deeppad_r101_480x480_80k_pascal_context.py
+++ configs/deeppad/deeppad_r101_480x480_80k_pascal_context.py
@@ -7,10 +7,11 @@ model = dict(
     pretrained='open-mmlab://resnet101_v1c',
     backbone=dict(depth=101),
     decode_head=dict(
+        type='DeepPadHead512',
         num_classes=60,
-        dyn_branch_ch=16,
-        mask_head_ch=16,
-        # pad_out_channel_factor=2,
+        dyn_branch_ch=8,
+        mask_head_ch=8,
+        pad_out_channel_factor=512/48,
     ),
     auxiliary_head=dict(num_classes=60),
     # test_cfg=dict(mode='slide', crop_size=(480, 480), stride=(320, 320))
