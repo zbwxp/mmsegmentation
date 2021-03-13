8b191e692086524721bf6bad5a69c01fd42e6cd4
zbwxp <zbwxpau@gmail.com>
2021-03-13 20:10:17 +1030

    new baseline '

--- configs/baseline/bilinear_noASPP_r50_512x512_40k_cityscapes.py
+++ configs/baseline/bilinear_noASPP_r50_512x512_40k_cityscapes.py
@@ -1,9 +1,9 @@
 _base_ = [
     '../_base_/models/deeppad_r50.py',
     '../_base_/datasets/cityscapes_512x512.py', '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_40k.py'
+    '../_base_/schedules/schedule_40k_warmup.py'
 ]
-norm_cfg = dict(type='BN', requires_grad=True)
+norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
     # pretrained='open-mmlab://resnet18_v1c',
     backbone=dict(norm_cfg=norm_cfg),
@@ -18,6 +18,7 @@ model = dict(
         # in_channels=512,
         # channels=128,
         # pad_out_channel_factor=128/12,
+        sampler=dict(type='OHEMPixelSampler', thresh=0.7, min_kept=100000)
     ),
     auxiliary_head=None
 )
