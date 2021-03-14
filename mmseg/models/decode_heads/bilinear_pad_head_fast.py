b6ccdb1dc14dec892e4b274cab79ff91ca7fbed0
zbwxp <zbwxpau@gmail.com>
2021-03-14 14:47:56 +1030

    rm pring:

--- mmseg/models/decode_heads/bilinear_pad_head_fast.py
+++ mmseg/models/decode_heads/bilinear_pad_head_fast.py
@@ -161,7 +161,7 @@ class BilinearPADHead_fast(ASPPHead):
     def forward(self, inputs):
         """Forward function."""
         x = self._transform_inputs(inputs)
-        print("input:", x.max())
+        # print("input:", x.max())
         aspp_outs = [
             resize(
                 self.image_pool(x),
@@ -203,7 +203,7 @@ class BilinearPADHead_fast(ASPPHead):
             outputs.append(output3)
             return outputs
 
-        print("output:", output.max())
+        # print("output:", output.max())
         return output
 
     def interpolate(self, x, x_cat=None, norm=None):
