80b4a0e193c77280cf4a5fe52440809df0621aa3
zbwxp <zbwxpau@gmail.com>
2021-03-15 10:36:45 +1030

    baseline no ASPP two version

--- configs/baseline/bilinear_noASPP_r50_512x512_40k_cityscapes_warmup_ohem.py
+++ configs/baseline/bilinear_noASPP_r50_512x512_40k_cityscapes_warmup_ohem.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../_base_/models/deeppad_r50.py',
     '../_base_/datasets/cityscapes_512x512.py', '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_40k.py'
+    '../_base_/schedules/schedule_40k_warmup.py'
 ]
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
@@ -18,7 +18,7 @@ model = dict(
         # in_channels=512,
         # channels=128,
         # pad_out_channel_factor=128/12,
-        # sampler=dict(type='OHEMPixelSampler', thresh=0.7, min_kept=100000)
+        sampler=dict(type='OHEMPixelSampler', thresh=0.7, min_kept=100000)
     ),
     auxiliary_head=None
 )
